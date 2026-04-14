ea3d4f7f23f83bef8422a076cc5b644fd65484a6
hohoho <66071582+HohohoCode@users.noreply.github.com>
2026-04-14 17:50:35 -0300

    Delete launch.sh

--- launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script para executar o programa sem terminal visível
-
-# Esconde o terminal e executa o programa
-nohup sudo python3 ~/Downloads/AI-SECURITY-SUITE-LINUX-MINT-E-UBUNTU-main/run.py > /dev/null 2>&1 &
-
-echo "✅ AI Security Suite iniciado em segundo plano!"
-echo "Para ver a janela, verifique o ícone na bandeja do sistema"
